4eef4ee3bb87235ae9d0baeb0415141b772b5c86
Tran Quoc Ngoc <kxhjr7gzyx@privaterelay.appleid.com>
2023-04-12 20:25:37 +0700

    feat: add themes

---
+++ src/themes/icons.js
@@ -0,0 +1,6 @@
+export const Icons = {
+  ic_location_on: require('../assets/icons/ic_location_on.png'),
+  ic_location_off: require('../assets/icons/ic_location_off.png'),
+  ic_settings: require('../assets/icons/ic_settings.png'),
+  ic_history: require('../assets/icons/ic_history.png'),
+};
