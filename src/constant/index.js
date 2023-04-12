fa27dfdf9a4158b594c9480c84d62f042395cd90
Tran Quoc Ngoc <kxhjr7gzyx@privaterelay.appleid.com>
2023-04-12 20:27:11 +0700

    feat: add resources

---
+++ src/constant/index.js
@@ -0,0 +1,22 @@
+export const MAP = {
+  latitude: 37.785834,
+  longitude: -122.406417,
+  latitudeDelta: 0.015,
+  longitudeDelta: 0.0121,
+};
+export const CAMERA_OPTION = {
+  quality: 0.75,
+  maxWidth: 800,
+  maxHeight: 1200,
+  includeBase64: true,
+};
+export const LIBRARY_OPTION = {
+  quality: 0.75,
+  maxWidth: 900,
+  maxHeight: 1300,
+  includeBase64: true,
+};
+export const SETTINGS_DEFAULT = {
+  assignedLocation: {latitude: 37.785834, longitude: -122.406457},
+  radius: 100,
+};
