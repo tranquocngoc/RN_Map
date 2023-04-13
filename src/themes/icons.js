f06214cac2df4e94db57c82647850779a74cb59a
Tran Quoc Ngoc <kxhjr7gzyx@privaterelay.appleid.com>
2023-04-13 17:13:18 +0700

    chore: add icon camera

--- src/themes/icons.js
+++ src/themes/icons.js
@@ -3,4 +3,5 @@ export const Icons = {
   ic_location_off: require('../assets/icons/ic_location_off.png'),
   ic_settings: require('../assets/icons/ic_settings.png'),
   ic_history: require('../assets/icons/ic_history.png'),
+  ic_camera: require('../assets/icons/ic_camera.png'),
 };
