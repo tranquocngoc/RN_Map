3c72dac7f943f8b4ba4171a5b38b45511218d506
Tran Quoc Ngoc <kxhjr7gzyx@privaterelay.appleid.com>
2023-04-13 17:15:21 +0700

    chore: fix option camera

--- src/constant/index.js
+++ src/constant/index.js
@@ -8,13 +8,11 @@ export const CAMERA_OPTION = {
   quality: 0.75,
   maxWidth: 800,
   maxHeight: 1200,
-  includeBase64: true,
 };
 export const LIBRARY_OPTION = {
   quality: 0.75,
   maxWidth: 900,
   maxHeight: 1300,
-  includeBase64: true,
 };
 export const SETTINGS_DEFAULT = {
   assignedLocation: {latitude: 37.785834, longitude: -122.406457},
